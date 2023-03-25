b966b7cfd843d35850a6b52a6c989017cb531f90
Kagwep <kagwepeter07@gmail.com>
2023-03-26 00:42:51 +0300

    add-token-module

--- substrate-contract/nyumba/lib.rs
+++ substrate-contract/nyumba/lib.rs
@@ -6,11 +6,13 @@
 #[ink::contract]
 mod nyumba {
 
-
+    
+    use erc20::Erc20;
     use ink::storage::{Mapping};
     use ink_prelude::string::String;
     use ink_prelude::vec::Vec;
     use ink_prelude::collections::BTreeMap;
+    
 
 
  
@@ -44,13 +46,14 @@ mod nyumba {
         token_symbol: String,
         token_name: String,
     }
-    
+     
     #[ink(storage)]
     pub struct Nyumba {
         users: Mapping<AccountId, UserData>,
         all_users: Vec<UserData>,
         assets: Mapping<String, AssetData>,
         all_assets:Vec<AssetData>,
+        erc20: Erc20,
     }
     
     impl Nyumba {
@@ -60,11 +63,13 @@ mod nyumba {
         
         #[ink(constructor)]
         pub fn new() -> Self {
+            let erc20 = Erc20::new(0);
             Self {
                 users: Mapping::new(),
                 all_users: Vec::new(),
                 assets: Mapping::new(),
                 all_assets: Vec::new(),
+                erc20: erc20,
             }}
 
             #[ink(message)]
@@ -119,7 +124,7 @@ mod nyumba {
 
 
             #[ink(message)]
-            pub fn register_asset(&mut self, property_owner: AccountId, 
+            pub fn register_asset(&mut self,
                                 property_name: String, property_location: String,
                                 property_value: u128,
                                 token_price: u128, token_symbol: String,property_id:String, 
@@ -130,9 +135,10 @@ mod nyumba {
                 }
                 let token_supply = property_value / token_price;
                 let new_property_id = property_id.clone();
+                let to_send = token_supply.clone();
 
                 let asset_data = AssetData {
-                    property_owner,
+                    property_owner: caller,
                     property_name,
                     property_location,
                     property_value,
@@ -145,8 +151,42 @@ mod nyumba {
                 };
                 self.assets.insert(&property_id, &asset_data);
                 self.all_assets.push(asset_data);
+                self.erc20.set_balance(to_send);
                 true
             }
+
+            #[ink(message)]
+            pub fn purchase_asset(&mut self, property_id: String, amount: u128) -> bool {
+                let buyer = self.env().caller();
+                
+                if !self.assets.get(&property_id).is_some() {
+                    return false;
+                }
+
+
+                let mut asset_data = self.assets.get(&property_id).unwrap().clone();
+
+                let tokens_to_buy = amount.clone();
+                if tokens_to_buy > asset_data.token_supply {
+                    return false;
+                }
+                let mut tokens_owned = asset_data.tokens_owned.clone();
+                let buyer_tokens_owned = tokens_owned.entry(buyer).or_insert(0);
+                *buyer_tokens_owned += tokens_to_buy;
+                let seller = asset_data.property_owner;
+  
+                let transfer_amount = amount.clone();
+                let result = self.erc20.transfer_from(seller.clone(), buyer.clone(), transfer_amount);
+                if result.is_err() {
+                    *buyer_tokens_owned -= tokens_to_buy;
+                    return false;
+                }
+
+                asset_data.token_supply -= tokens_to_buy;
+                self.assets.insert(&property_id, &asset_data);
+                true
+            }
+
 
 
             #[ink(message)]
@@ -156,7 +196,7 @@ mod nyumba {
 
         
             #[ink(message)]
-            pub fn update_asset(&mut self,  property_owner: AccountId, 
+            pub fn update_asset(&mut self,
                         property_name: String, property_location: String,
                         property_value: u128,
                         token_price: u128, token_symbol: String,property_id:String, 
@@ -175,8 +215,6 @@ mod nyumba {
 
                 let tokens_owned = asset_data.tokens_owned.clone();
 
-
-                asset_data.property_owner = property_owner;
                 asset_data.property_name = property_name;
                 asset_data.property_location = property_location;
                 asset_data.property_value = property_value;
@@ -202,6 +240,17 @@ mod nyumba {
                 true
             }
 
+
+            #[ink(message)]
+            pub fn get_total_token_supply(&self) -> Balance {
+                self.erc20.total_supply()
+            }
+
+            #[ink(message)]
+            pub fn get_balance_of(&self,account_id:AccountId) -> Balance {
+                self.erc20.balance_of(account_id)
+            }
+
     
     }
 
@@ -220,6 +269,7 @@ mod nyumba {
             let account_id = AccountId::from([1; 32]);
             let name = String::from("Alice");
             let email = String::from("123 Main St.");
+            let caller = account_id.clone();
           
 
             // Action
@@ -227,6 +277,19 @@ mod nyumba {
 
             // Assert
             assert_eq!(owner_register.get_user(account_id), Some(UserData{name, email,account_id}));
+            
+            // assert token supply
+
+            let total_bal = owner_register.get_total_token_supply();
+
+            assert_eq!(total_bal,0);
+
+
+            
+
+            let balance = owner_register.get_balance_of(caller);
+  
+            assert_eq!(balance,0)
         }
 
         #[ink::test]
@@ -241,7 +304,7 @@ mod nyumba {
             user_register.register_user(name.clone(), email.clone());
 
             let users = user_register.get_all_users();
-            println!("All users: {:?}", users);
+            
             assert_eq!(users[0].name, "Alice");
 
             // Assert
@@ -289,6 +352,18 @@ mod nyumba {
             let token_name = String::from("Polkadot Reak Estate");
             let token_supply:u128 = property_value / token_price;
             let new_property_id = property_id.clone();
+            let caller = property_owner.clone();
+
+            let property_id_1 = String::from("prop2");
+            let property_name_1 = String::from("Twiga Devs Prop2");
+            let property_location_1 = String::from("Remote2");
+            let property_value_1:u128 = 40000000;
+            let token_price_1:u128 = 30000;
+            let token_symbol_1 = String::from("PRE1");
+            let token_name_1 = String::from("Polkadot Reak Estate1");
+            let token_supply_1:u128 = property_value_1 / token_price_1;
+            let new_property_id_1 = property_id.clone();
+            let caller_1 = property_owner.clone();
 
 
             // property_owner: AccountId, 
@@ -297,10 +372,11 @@ mod nyumba {
             // token_price: u128, token_symbol: String,property_id:String, 
             //token_name: String
 
+            let total_tok = token_supply.clone() + token_supply_1;
+
 
 
             let register = contract.register_asset(
-                property_owner.clone(),
                 property_name.clone(),
                 property_location.clone(),
                 property_value.clone(),
@@ -308,6 +384,17 @@ mod nyumba {
                 token_symbol.clone(),
                 property_id.clone(), 
                 token_name.clone()
+            );
+
+
+            let register1 = contract.register_asset(
+                property_name_1.clone(),
+                property_location_1.clone(),
+                property_value_1.clone(),
+                token_price_1.clone(),
+                token_symbol_1.clone(),
+                property_id_1.clone(), 
+                token_name_1.clone()
             );
 
 
@@ -322,6 +409,30 @@ mod nyumba {
                                                                     token_price,
                                                                     token_symbol,
                                                                     token_name,}));
+
+        let total_bal = contract.get_total_token_supply();
+
+        assert_eq!(total_bal,0);
+
+        let balance = contract.get_balance_of(caller);
+
+        assert_eq!(balance,total_tok.clone());
+
+
+        let purchase = contract.purchase_asset("prop2".to_string(), 100);
+
+        assert_eq!(purchase,true);
+
+        let new_caller = AccountId::from([1; 32]);
+
+        let new_balance = contract.get_balance_of(new_caller);
+
+        assert_eq!(new_balance,total_tok);
+
+        let retrieved_asset_data_sold = contract.get_asset("prop2".to_string()).unwrap();
+
+        assert_eq!(retrieved_asset_data_sold.token_supply, (token_supply_1 - 100));
+ 
 
         }
 
@@ -343,7 +454,6 @@ mod nyumba {
 
             // Action
             contract.register_asset(
-                property_owner.clone(),
                 property_name.clone(),
                 property_location.clone(),
                 property_value.clone(),
@@ -358,7 +468,6 @@ mod nyumba {
             let property_name_updated = String::from("Twiga Devs");
 
             let result = contract.update_asset(
-                                                property_owner.clone(),
                                                 property_name_updated.clone(),
                                                 property_location.clone(),
                                                 property_value_updated.clone(),
